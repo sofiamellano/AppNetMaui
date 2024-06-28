39854ee68d4e54f7fef47eb5f0a34e7707d6606d
sofiamellano <sofiimellano@gmail.com>
2024-06-28 15:54:53 -0300

    Agregamos InicioPages cargandose inicialmente

--- PrimeraMauiApp/App.xaml.cs
+++ PrimeraMauiApp/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace PrimeraMauiApp
+﻿using PrimeraMauiApp.Pages;
+
+namespace PrimeraMauiApp
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new InicioPages();
         }
     }
 }
