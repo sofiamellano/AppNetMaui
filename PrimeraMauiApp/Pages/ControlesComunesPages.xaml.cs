a6e37536af0fa1c3ca2857232736a64b86c662b1
sofiamellano <sofiimellano@gmail.com>
2024-08-16 15:58:01 -0300

    editor, searchbar, listview y binding

--- PrimeraMauiApp/Pages/ControlesComunesPages.xaml.cs
+++ PrimeraMauiApp/Pages/ControlesComunesPages.xaml.cs
@@ -1,9 +1,21 @@
+using System.Collections.ObjectModel;
+
 namespace PrimeraMauiApp.Pages;
 
 public partial class ControlesComunesPages : ContentPage
 {
+	public ObservableCollection<string> Frutas { get; set; } = new ObservableCollection<string>();
 	public ControlesComunesPages()
 	{
 		InitializeComponent();
-	}
+        Frutas = new ObservableCollection<string>
+        {
+            "Manzana",
+            "Banana",
+            "Naranja",
+            "Fresa",
+            "Uva"
+        };
+        BindingContext = this;
+    }
 }
